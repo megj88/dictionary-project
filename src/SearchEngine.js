aec9cb3b0f73405e6ab73aaabf7acf429e64b815
Megan Jones <115540207+megj88@users.noreply.github.com>
2023-02-25 20:12:27 +0100

    add axios and api call

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,14 +1,24 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function SearchEngine() {
     let [keyword, setKeyword] = useState("");
     function handleKeywordChange(event) {
         setKeyword(event.target.value);
     }
+
+function handleResponse(response) {
+    console.log(response.data[0])
+}
+
     function search(event) {
         event.preventDefault();
-        alert(`Searching for ${keyword}`);
+
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+        axios.get(apiUrl).then(handleResponse);
     }
+
+
     return (
         <div className="Search">
            <form onSubmit={search}>
