9cd6893ef5664f9ca90509cd5fd7226a2cb2488f
Megan Jones <115540207+megj88@users.noreply.github.com>
2023-03-05 11:42:34 +0100

    make images responsive

--- src/Photos.js
+++ src/Photos.js
@@ -9,7 +9,7 @@ export default function Photos(props) {
             <div className="row">
             {props.photos.map(function(photo, index){
                 return (
-                    <div className="col-4" key={index}>
+                    <div className="col-lg-4 col-md-6 col-sm-10" key={index}>
                     <a href={photo.src.original} target="_blank" rel="noopener noreferrer">
                    <img src={photo.src.landscape} className="img-fluid" alt={photo.alt}/> </a>
                    </div>
